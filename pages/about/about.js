903cf82b951837b06e7901b443dd04e53d8399a0
neil <neilhellohello.gmail.com>
2020-05-28 16:43:38 +0800

    全局配置一波,基本三项,pages,window,tabBar

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,66 +1,27 @@
-// about/about.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    name : "lq",
+    age: 18,
+    students: [
+      {id:110, name:"lobe0", age: 30},
+      {id:111, name:"lobe1", age: 31},
+      {id:112, name:"lobe2", age: 32}
+    ],
+    counter:0,
   },
+  handleBtnClick(){
+    // 1.错误做法： 界面是不会刷新的
+    // this.data.counter += 1
+    // console.log(this.data.counter)
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+    // 2.this.setData()
+    this.setData({
+      counter: this.data.counter + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleBtnClick2(){
+    this.setData({
+      counter: this.data.counter - 1
+    })
   }
 })
